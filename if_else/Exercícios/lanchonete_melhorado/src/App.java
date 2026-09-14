5727b6c5c01db03edd2921e8413e6d48112cb0b7
KyleNdaka <kylendaka@gmail.com>
2026-09-14 20:06:19 -0300

    arrumando com while

--- if_else/Exercícios/lanchonete_melhorado/src/App.java
+++ if_else/Exercícios/lanchonete_melhorado/src/App.java
@@ -2,41 +2,63 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(
-                " BEM - VINDO A NOSSA LANCHONETE\n HARBURGUER: R$18,00\n REFRI: R$7,00\n DIGITE [1] PARA PEGAR HARMBURGUERES E REFRI\n DIGITE [2] PARA PEGAR SÓ HAMBURGUER\n DIGITE [3] PARA PEGAR SÓ REFRI");
-
         Scanner input = new Scanner(System.in);
 
-        String resp = input.next();
-        double hamburguer, total, refri;
-        int quantidade_refri, quantidade_hamburguer;
+        System.out.println("""
+                ********** LANCHONETE **********
+                BEM - VINDO A NOSSA LANCHONETE
+                Hamburguer: R$18.00
+                Refri: R$7.00
+                Digite...
+                [1] Para pegar hamburguer e refri
+                [2] Para pegar só hamburger
+                [3] Para pegar só refri
+                """);
+        double hamburguer, total_refri, total_hamburguer, refri, total;
+        int quantidade_refri, quantidade_hamburguer, opcao;
         refri = 7.00;
         hamburguer = 18.00;
+        total_hamburguer = 0;
+        total_refri = 0;
         total = 0;
 
-        if (resp.equals("1")) {
-            System.out.println("Digite quantos Hamburguers você quer: ");
-            quantidade_hamburguer = input.nextInt();
-            total += quantidade_hamburguer * hamburguer;
-            System.out.printf("O total é %.2f\n", total);
+        opcao = input.nextInt();
 
-            System.out.println("Digite quantos refrigerantes você quer: ");
-            quantidade_refri = input.nextInt();
-            total += quantidade_refri * refri;
-            System.out.printf("O total é %.2f", total);
+        switch (opcao) {
+            case 1: {
+                System.out.println("Digite quantos Hamburguers você quer: ");
+                quantidade_hamburguer = input.nextInt();
+                total_hamburguer += quantidade_hamburguer * hamburguer;
 
-        } else if (resp.equals("2")) {
-            System.out.println("Digite quantos Hamburguers você quer: ");
-            quantidade_hamburguer = input.nextInt();
-            total += quantidade_hamburguer * hamburguer;
-            System.out.printf("O total é %.2f\n", total);
+                System.out.println("Digite quantos refrigerantes você quer: ");
+                quantidade_refri = input.nextInt();
+                total_refri += quantidade_refri * refri;
+                total = total_hamburguer + total_refri;
+                System.out.printf("O hamburger ficou R$%.2f\nO refri R$%.2f\nO total é R$%.2f\n", total_hamburguer, total_refri, total);
+                        
+            }
+                break;
 
-        } else if (resp.equals("3")) {
-            System.out.println("Digite quantos refrigerantes você quer: ");
-            quantidade_refri = input.nextInt();
-            total += quantidade_refri * refri;
-            System.out.printf("O total é %.2f\n", total);
+            case 2: {
+                System.out.println("Digite quantos Hamburguers você quer: ");
+                quantidade_hamburguer = input.nextInt();
+                total_hamburguer += quantidade_hamburguer * hamburguer;
+                System.out.printf("O total é R$%.2f\n", total_hamburguer);
+            }
+                break;
+
+            case 3: {
+                System.out.println("Digite quantos refrigerantes você quer: ");
+                quantidade_refri = input.nextInt();
+                total_refri += quantidade_refri * refri;
+                System.out.printf("O total é R$%.2f\n", total_refri);
+            }
+                break;
+
+            default:
+                System.out.println("Opçãop inválida!\nEscolha de 1 a 3");
         }
+
         input.close();
 
     }
