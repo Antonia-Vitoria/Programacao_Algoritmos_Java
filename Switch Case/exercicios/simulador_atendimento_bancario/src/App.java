01a108cd1edfa7b1e5c4ef21c65d0aca991037d4
Antonia Vitoria <antoniacaetano2009@gmail.com>
2026-09-14 00:05:47 -0300

    Arumando sistema-bancario

--- Switch Case/exercicios/simulador_atendimento_bancario/src/App.java
+++ Switch Case/exercicios/simulador_atendimento_bancario/src/App.java
@@ -4,54 +4,118 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 
-        double deposito, total, saque;
-        int saldo, opcao;
-        saldo = 1000;
+        boolean cont = true;
+        while (cont) {
 
-        System.out.println("""
-                Simulador de Atendimento Bancário
-                1 - Para consultar saldo
-                2 - Para sacar dinheiro
-                3 - Para depositar dinheiro
-                4 - Para encerrar atendimento
-                 """);
+            double deposito, total, saque;
+            int saldo, opcao;
+            saldo = 1000;
 
-        opcao = input.nextInt();
+            System.out.println("""
+                    Simulador de Atendimento Bancário
+                    1 - Para consultar saldo
+                    2 - Para sacar dinheiro
+                    3 - Para depositar dinheiro
+                    4 - Para encerrar atendimento
+                     """);
 
-        switch (opcao) {
-            case 1: {
-                System.out.printf("Seu saldo é de %d\n", saldo);
+            opcao = input.nextInt();
 
-            }
-                break;
-            case 2: {
-                System.out.println("Quanto você quer sacar?\n");
-                saque = input.nextDouble();
+            switch (opcao) {
+                case 1: {
+                    System.out.printf("Seu saldo é de R$%d\n", saldo);
 
-                if (saque > saldo) {
-                    System.out.printf("Opção invalida! \nValor maior que o saldo\n Você tem no seu saldo R$%d\n",
-                            saldo);
-                } else {
-                    System.out.printf("Você sacou R$%.2f\n", saque);
-                    total = saldo - saque;
+                    System.out.println("Digite 'E' para encerrar ou 'R' para reiniciar: ");
+                    String resp = input.next();
+
+                    if (resp.equalsIgnoreCase("E")) {
+                        cont = false;
+                        System.out.println("Programa encerrado.");
+                    } else {
+                        
+                    }
+
                 }
-            }
-                break;
-            case 3: {
-                System.out.println("Digite quanto você quer depositar:");
-                deposito = input.nextDouble();
-                total = saldo + deposito;
-                System.out.printf("Seu saldo agora é de: R$%.2f", total);
-            }
-                break;
-            case 4: {
-                System.out.println("Atendimento terminado!\n");
-            }
-                break;
-            default: {
-                System.out.println("Essa opção não existe");
-            }
+                    break;
 
+                case 2: {
+                    System.out.printf("Quanto você quer sacar?\nLEMBRANDO: Seu saldo é de R$%d\n", saldo);
+                    saque = input.nextDouble();
+                    total = saldo - saque;
+
+                    if (saque > saldo) {
+                        System.out.printf("Opção invalida! \nValor maior que o saldo\nVocê tem no seu saldo R$%d\n", saldo);
+                                
+                        System.out.println("Digite 'E' para encerrar ou 'R' para reiniciar: ");
+                        String resp = input.next();
+
+                        if (resp.equalsIgnoreCase("E")) {
+                            cont = false;
+                            System.out.println("Programa encerrado.");
+                        } else {
+                            
+                        }
+
+                    } else if (saque <= saldo) {
+                        System.out.printf("Você sacou R$%.2f\nAgora seu saldo é de R$%.2f\n", saque, total);
+                        System.out.println("Digite 'E' para encerrar ou 'R' para reiniciar: ");
+                        String resp = input.next();
+
+                        if (resp.equalsIgnoreCase("E")) {
+                            cont = false;
+                            System.out.println("Programa encerrado.");
+                        } else {
+                            
+                        }
+                    }
+
+                }
+                    break;
+
+                case 3: {
+                    System.out.println("Digite quanto você quer depositar:");
+                    deposito = input.nextDouble();
+                    total = saldo + deposito;
+                    System.out.printf("Seu saldo agora é de: R$%.2f\n", total);
+
+                    System.out.println("Digite 'E' para encerrar ou 'R' para reiniciar: ");
+                    String resp = input.next();
+
+                    if (resp .equalsIgnoreCase("E")) {
+                        cont = false;
+                        System.out.println("Programa encerrado.");
+
+                    } else {
+                        
+                    }
+                }
+                    break;
+
+                case 4: {
+                    System.out.println("Atendimento terminado!\n");
+                    cont = false;
+                    break;
+                }
+
+                default: {
+                    System.out.println("Essa opção não existe");
+
+                    System.out.println("Digite 'E' para encerrar ou 'R' para reiniciar: ");
+                    String resp = input.next();
+
+                    if (resp.equalsIgnoreCase("E")) {
+                        cont = false;
+                        System.out.println("Programa encerrado.");
+                    } else {
+                    
+                    }
+
+                }
+                    break;
+                
+            }
+            
         }
+        input.close();
     }
 }
